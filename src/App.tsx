dd6f9f2cf44dcb355e39e8449787cb6f8a2ffc6f
Mark Sorce <marksorce@gmail.com>
2023-02-19 18:29:21 -0800

    implement useContext for todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TodoFilter from "./components/Todofilter";
 import Todos from "./components/Todos";
-
+import TodoContext from "./TodoContext";
+import { TodoItemData } from "./types";
 const App = () => {
   const [filter, setFilter] = useState("");
+  const [todos, setTodos] = useState<TodoItemData[]>([
+    { todo: "", completed: false, id: "" },
+  ]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onFilterChange = (newFilter: string) => {
-    setFilter(newFilter);
+  const handleUpdateTodo = (i: string) => {
+    const newTodos = todos.slice();
+    newTodos.forEach((todo) => {
+      const { id } = todo;
+      if (id === i) todo.completed = !todo.completed;
+    });
+    setTodos(newTodos);
   };
 
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://dummyjson.com/todos")
+      .then((todos) => todos.json())
+      .then(({ todos }) => setTodos(todos))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
-    <div className="App">
-      <TodoFilter filter={filter} onFilterChange={onFilterChange} />
-      <Todos filter={filter} />
-    </div>
+    <TodoContext.Provider
+      value={{
+        filter,
+        setFilter,
+        todos,
+        setTodos,
+        isLoading,
+        setIsLoading,
+        handleUpdateTodo,
+      }}
+    >
+      <div className="App">
+        <TodoFilter />
+        <Todos />
+      </div>
+    </TodoContext.Provider>
   );
 };
 
